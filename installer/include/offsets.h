7dd20fe3721312ca44b74372ea9bccaf82e957c7
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-02 16:42:14 -0600

    Added 11.00 Offsets

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -2,42 +2,42 @@
 #define __OFFSETS_H__
 #pragma once
 
-// 9.00
+// 11.00
 #define	XFAST_SYSCALL_addr              0x000001C0
 
 // Names - Data
-#define PRISON0_addr                    0x0111F870
-#define ROOTVNODE_addr                  0x021EFF20
-#define PMAP_STORE_addr                 0X01B904B0
-#define DT_HASH_SEGMENT_addr            0x00C2C520
+#define PRISON0_addr                    0x0111F830
+#define ROOTVNODE_addr                  0x02116640
+#define PMAP_STORE_addr                 0X02162A88
+#define DT_HASH_SEGMENT_addr            0x00CEAC00
 
 // Functions
-#define pmap_protect_addr               0x0012FA90
-#define pmap_protect_p_addr             0x0012FAD7
+#define pmap_protect_addr               0x00116CB0
+#define pmap_protect_p_addr             0x00116CF7
 
 // Patches
 // debug menu error
-#define debug_menu_error_patch1         0x004E8E48
-#define debug_menu_error_patch2         0x004EA12F
+#define debug_menu_error_patch1         0x004EE328
+#define debug_menu_error_patch2         0x004EF3EE
 
 // disable signature check
-#define disable_signature_check_patch   0x006885C0
+#define disable_signature_check_patch   0x00684EB0
 
 // enable debug RIFs
-#define enable_debug_rifs_patch1        0x00650430
-#define enable_debug_rifs_patch2        0x00650460
+#define enable_debug_rifs_patch1        0x0064BFD0
+#define enable_debug_rifs_patch2        0x0064C000
 
 // allow sys_dynlib_dlsym in all processes
-#define sys_dynlib_dlsym_patch1         0x0023B67F
-#define sys_dynlib_dlsym_patch2         0x00221B40
+#define sys_dynlib_dlsym_patch1         0x0064BFD0
+#define sys_dynlib_dlsym_patch2         0x0064C000
 
 // sdk version spoof - enable all VR fws
-#define sdk_version_patch               0x1A6CDF0
+#define sdk_version_patch               0x00D241A0
 
 // enable debug log
-#define enable_debug_log_patch          0x000B7B17
+#define enable_debug_log_patch          0x002FCCB7
 
 // enable uart output
-#define enable_uart_patch               0x0152BF5D
+#define enable_uart_patch               0x00D2CFF5
 
 #endif
