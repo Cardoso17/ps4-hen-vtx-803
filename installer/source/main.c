c5680ea660fa8c0744aebc9bce0c9a0b53294c29
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-08 08:19:14 -0600

    Fixed games not loading

--- installer/source/main.c
+++ installer/source/main.c
@@ -12,16 +12,8 @@ extern unsigned kpayload_size;
 
 int install_payload(struct thread *td, struct payload_info* payload_info)
 {
-	struct ucred* cred;
-	struct filedesc* fd;
-
-	fd = td->td_proc->p_fd;
-	cred = td->td_proc->p_ucred;
 
 	uint8_t* kernel_base = (uint8_t*)(__readmsr(0xC0000082) - XFAST_SYSCALL_addr);
-	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
-	void** got_prison0 = (void**)&kernel_ptr[PRISON0_addr];
-	void** got_rootvnode = (void**)&kernel_ptr[ROOTVNODE_addr];
 
 	void (*pmap_protect)(void * pmap, uint64_t sva, uint64_t eva, uint8_t pr) = (void *)(kernel_base + pmap_protect_addr);
 	void *kernel_pmap_store = (void *)(kernel_base + PMAP_STORE_addr);
@@ -34,31 +26,8 @@ int install_payload(struct thread *td, struct payload_info* payload_info)
 	if (!payload_data || payload_size < sizeof(payload_header) || payload_header->signature != 0x5041594C4F414458ull)
 		return -1;
 
-	cred->cr_uid = 0;
-	cred->cr_ruid = 0;
-	cred->cr_rgid = 0;
-	cred->cr_groups[0] = 0;
-
-	cred->cr_prison = *got_prison0;
-	fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
-
 	// Use "kmem" for all patches
-    uint8_t *kmem;
-
-	// escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
-	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
-
-	// sceSblACMgrIsSystemUcred
-	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
-	*sonyCred = 0xffffffffffffffff;
-
-	// sceSblACMgrGetDeviceAccessType
-	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
-	*sceProcType = 0x3801000000000013; // Max access
-
-	// sceSblACMgrHasSceProcessCapability
-	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
-	*sceProcCap = 0xffffffffffffffff; // Sce Process
+        uint8_t *kmem;
 
 	// Disable write protection
 	uint64_t cr0 = readCr0();
