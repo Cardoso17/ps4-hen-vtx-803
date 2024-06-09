2e0bac7399bec43db728e1af61c51fc2ca0ca1e3
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-09 10:15:19 -0600

    added 950-960

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ pushd installer
 make
 popd
 
-rm -f ps4-hen-950-vtx.bin
-cp installer/installer.bin ps4-hen-950-vtx.bin
+rm -f ps4-hen-950-960-vtx.bin
+cp installer/installer.bin ps4-hen-950-960-vtx.bin
