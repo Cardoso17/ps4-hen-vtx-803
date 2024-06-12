1aaf4c15ca64834216acdf625943bf13ea06beb7
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-11 18:59:14 -0600

    fixed build

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ pushd installer
 make
 popd
 
-rm -f ps4-hen-950-960-vtx.bin
-cp installer/installer.bin ps4-hen-950-960-vtx.bin
+rm -f ps4-hen-950-960-PPPwn-vtx.bin
+cp installer/installer.bin ps4-hen-950-960-PPPwn-vtx.bin
