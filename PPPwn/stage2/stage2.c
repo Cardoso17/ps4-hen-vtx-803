e8fae43da8b4eac35a33dc74438e62320cbee4d0
jcardoso17 <jpcardoso97@gmail.com>
2024-06-13 04:21:36 +0100

    Fix

--- PPPwn/stage2/stage2.c
+++ PPPwn/stage2/stage2.c
@@ -41,6 +41,26 @@ typedef struct {
 
 struct sysent *sysents;
 
+size_t strlen(const char * s) {
+  const char * t = s;
+  while (* t)
+    t++;
+  return t - s;
+}
+
+
+int memcmp(const void * str1,
+  const void * str2, size_t count) {
+  const unsigned char * s1 = (const unsigned char * ) str1;
+  const unsigned char * s2 = (const unsigned char * ) str2;
+
+  while (count--> 0) {
+    if ( * s1++ != * s2++)
+      return s1[-1] < s2[-1] ? -1 : 1;
+  }
+  return 0;
+}
+
 static inline uint64_t rdmsr(u_int msr) {
   uint32_t low, high;
   asm volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(msr));
